8f71b207bbd8e000a3ac312eb6e61544f6c6ab24
Javier Tapia <jgtapiab@telconet.ec>
2023-12-07 00:59:30 -0500

    Section 7: Spring MockMVC Test with Mockito Finished - Date: 07/12/2023

--- src/main/java/udemy/course/spring6restmvc/controller/CustomerController.java
+++ src/main/java/udemy/course/spring6restmvc/controller/CustomerController.java
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import udemy.course.spring6restmvc.model.Customer;
 import udemy.course.spring6restmvc.services.CustomerService;
@@ -25,12 +24,14 @@ import udemy.course.spring6restmvc.services.CustomerService;
 @Slf4j
 @AllArgsConstructor
 @RestController
-@RequestMapping("/api/v1/customer")
 public class CustomerController {
+
+    public static final String CUSTOMER_PATH = "/api/v1/customer";
+    public static final String CUSTOMER_PATH_ID = CUSTOMER_PATH + "/{id}";
 
     private final CustomerService customerService;
 
-    @PatchMapping("{id}")
+    @PatchMapping(CUSTOMER_PATH_ID)
     public ResponseEntity<?> updateCustomerPatchById(@PathVariable("id") UUID id,
                                                      @RequestBody Customer customer) {
         customerService.patchCustomerById(id, customer);
@@ -38,14 +39,14 @@ public class CustomerController {
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
-    @DeleteMapping("{id}")
+    @DeleteMapping(CUSTOMER_PATH_ID)
     public ResponseEntity<?> deleteById(@PathVariable("id") UUID id) {
         customerService.deleteById(id);
         log.debug("Deleted customer with id: " + id);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
-    @PutMapping("{id}")
+    @PutMapping(CUSTOMER_PATH_ID)
     public ResponseEntity<?> updateById(@PathVariable("id") UUID id,
                                         @RequestBody Customer customer) {
         customerService.updateCustomerById(id, customer);
@@ -53,7 +54,7 @@ public class CustomerController {
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
-    @PostMapping
+    @PostMapping(CUSTOMER_PATH)
     public ResponseEntity<?> handlePost(@RequestBody Customer customer) {
         Customer savedCustomer = customerService.saveNewCustomer(customer);
 
@@ -65,13 +66,13 @@ public class CustomerController {
         return new ResponseEntity<>(headers, HttpStatus.CREATED);
     }
 
-    @GetMapping
+    @GetMapping(CUSTOMER_PATH)
     public List<Customer> listCustomers() {
         log.debug("Listing customers - In controller...");
         return customerService.listCustomers();
     }
 
-    @GetMapping("/{id}")
+    @GetMapping(CUSTOMER_PATH_ID)
     public Customer getCustomerById(@PathVariable("id") UUID id) {
         log.debug("Getting customer by id - In controller...");
         return customerService.getCustomerById(id);
